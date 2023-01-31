329d9a61b8a8ca019182c1a43591f751a7047f8d
maatzie <veronikazdanovich@mail.ru>
2023-01-31 15:48:59 +0100

    Project setup + header and footer added

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return <nav className=" indigo lighten-1">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Fortnite Shop</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="https://github.com/maatzie/FortniteShop">Repo</a></li>
+      </ul>
+    </div>
+  </nav>
+
+}
+
+export {Header}
