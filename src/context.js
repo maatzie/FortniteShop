4d331fdf8220fce87686a94af655c792699a6d4f
maatzie <veronikazdanovich@mail.ru>
2023-02-27 23:26:06 +0100

    reducer and context provider logic changed

--- src/context.js
+++ src/context.js
@@ -23,37 +23,20 @@ export const ContextProvider = ({children}) => {
     }
 
     value.addToBasket = (item) => {
-        
-        let itemIndex = value.order.findIndex(p => p.id === item.id);
-        
-        if(itemIndex < 0) {
-            
-            dispatch( {type: 'ADD_TO_BASKET', payload: {order: [{...item, quantity: 1}, ...value.order], alertName: item.name}});
-        } else {
-            const newOrder = value.order.map((orderItem, index) => {
-                if(index === itemIndex) {
-                    return {...orderItem, quantity: orderItem.quantity + 1}
-                } else {
-                    return orderItem;
-                }
-            });
-            dispatch( {type: 'ADD_TO_BASKET', payload: {order: newOrder, alertName: item.name}});
-        }
+        dispatch({type: 'ADD_TO_BASKET', payload: {item: item}});
     }
     
     value.incQuantity = (itemId) => {
-        const newOrder = value.order.map((el) => {
-            return el.id === itemId ? {...el, quantity: el.quantity + 1} : el;
-        });
-        dispatch({type: "INC_QUANTITY", payload: {order: newOrder}});
+        dispatch({type: "INC_QUANTITY", payload: {itemId: itemId}});
     }
     
     
     value.decQuantity = (itemId) => {
-        const newOrder = value.order.map((el) => {
-            return el.offerId === itemId ? {...el, quantity: el.quantity - 1} : el;
-        });
-        dispatch({type: "DEC_QUANTITY", payload: {order: newOrder}});
+        dispatch({type: "DEC_QUANTITY", payload: {itemId: itemId}});
+    }
+
+    value.handleBasketShow = () => {
+        dispatch({type: "HANDLE_BASKET_SHOW", payload: {isBasketShow: !value.isBasketShow}})
     }
     
     return <ShopContext.Provider value={value}>
--- src/reducer.js
+++ src/reducer.js
@@ -11,21 +11,50 @@ export function reducer(state, {type, payload}) {
                 order: state.order.filter(el => el.offerId !== payload.id),
             }
         case 'ADD_TO_BASKET':
-            return {
-                ...state, 
-                order: payload.order,
-                alertName: payload.alertName,
+            let itemIndex = state.order.findIndex(p => p.id === payload.item.id);
+            if(itemIndex < 0) {
+                return {
+                    ...state,
+                    order: [{...payload.item, quantity: 1}, ...state.order],
+                    alertName: payload.item.name
+                }
+            } else {
+                const newOrder = state.order.map((orderItem, index) => {
+                    if(index === itemIndex) {
+                        return {...orderItem, quantity: orderItem.quantity + 1}
+                    } else {
+                        return orderItem;
+                    }
+                });
+                return {
+                    ...state,
+                    order: newOrder,
+                    alertName: payload.item.name
+                }
             }
+
         case 'INC_QUANTITY': {
+            const newOrder = state.order.map((el) => {
+                return el.id === payload.itemId ? {...el, quantity: el.quantity + 1} : el;
+            });
             return {
                 ...state,
-                order: payload.order,
+                order: newOrder,
             }
         }
         case 'DEC_QUANTITY': {
+            const newOrder = state.order.map((el) => {
+                return el.id === payload.itemId ? {...el, quantity: el.quantity - 1} : el;
+            });
             return {
                 ...state,
-                order: payload.order,
+                order: newOrder.filter(el => el.quantity > 0),
+            }
+        }
+        case 'HANDLE_BASKET_SHOW': {
+            return {
+                ...state,
+                isBasketShow: payload.isBasketShow,
             }
         }
         default:
