38f122abf50ceca2634d76a3e327cba5207b1b50
maatzie <veronikazdanovich@mail.ru>
2023-03-01 00:56:31 +0100

    Shop component clean up

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -10,51 +10,6 @@ import { ShopContext } from "../context";
 function Shop () {
     const {loading, order, isBasketShow, alertName, setGoods} = useContext(ShopContext);
 
-    // const handleBasketShow = (item) => {
-    //     setIsBasketShow(!isBasketShow);
-    // }
-
-    // const addToBasket = (item) => {
-    //     let itemIndex = order.findIndex(p => p.id === item.id);
-        
-    //     if(itemIndex < 0) {
-    //         setOrder([{...item, quantity: 1}, ...order]);
-    //     } else {
-    //         const newOrder = order.map((orderItem, index) => {
-    //             if(index === itemIndex) {
-    //                 return {...orderItem, quantity: orderItem.quantity + 1}
-    //             } else {
-    //                 return orderItem;
-    //             }
-    //         });
-    //         setOrder(newOrder);
-    //     }
-    //     setAlertName(item.name);
-        
-    // };
-    
-    // const removeFromBasket = (id) => {
-    //     const newOrder = order.filter(el => el.id !== id);
-    //     setOrder(newOrder);
-    // }
-
-    // const incQuantity = (itemId) => {
-    //     const newOrder = order.map((el) => {
-    //         return el.id === itemId ? {...el, quantity: el.quantity + 1} : el;
-    //     });
-    //     setOrder(newOrder);
-    // };
-    // const decQuantity = (itemId) => {
-    //     const newOrder = order.map((el) => {
-    //         return el.id === itemId ? {...el, quantity: el.quantity - 1} : el;
-    //     });
-    //     setOrder(newOrder.filter(el => el.quantity > 0));
-    // };
-
-    // const closeAlert = () => {
-    //     setAlertName("");
-    // }
-    
     useEffect(function getGoods () {
         fetch(API_URL, {
             headers: {
